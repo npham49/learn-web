6227ce4a88aecba5bc7ca96c8916b1f514808c51
npham49 <nguyenphamswork@gmail.com>
2024-03-11 09:28:37 -0700

    bootstraped vue project with Options API

---
+++ vue-project/src/components/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home-page">
+    <h1>
+      Home Page
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  data() {
+    return {}
+  },
+  methods: {}
+})
+</script>
+
+<style lang="css" scoped>
+  .home-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -1,4 +1,6 @@
 import './assets/main.css'
+import '@bcgov/bc-sans/css/BCSans.css';
+import './assets/scss/style.scss';
 
 import { createApp } from 'vue'
 import App from './App.vue'
