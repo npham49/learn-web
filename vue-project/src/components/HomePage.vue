8a8a6d0977740c54ec97c412d5c5f380413bd2fc
npham49 <nguyenphamswork@gmail.com>
2024-03-13 08:58:44 -0700

    used vue options API with Objects

--- vue-project/src/components/HomePage.vue
+++ vue-project/src/components/HomePage.vue
@@ -1,27 +1,80 @@
 <template>
-  <div class="home-page">
-    <h1>
-      Home Page
-    </h1>
-    <button type="button" class="btn btn-primary">Primary</button>
-  </div>
+  <section>
+    <div class="nav-bar"></div>
+    <h1>Relecloud Galaxy Tours</h1>
+
+    <div>
+      <h2>Welcome!</h2>
+      <UserDisplay :user="user" ></UserDisplay>
+      <h2>{{ cruise.name }}</h2>
+      <div>{{ cruise.description }}</div>
+      <hr />
+
+      <div class="row">
+        <div>
+          <!-- TODO: Add booking-form -->
+        </div>
+        <div>
+          <!-- TODO: Add booking-list -->
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import UserDisplay from './user/UserDisplay.vue'
 export default defineComponent({
   data() {
-    return {}
+    return {
+      cruise: {
+        name: 'Cruise to the moon',
+        description:
+          'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+        cabins: [
+          { name: 'Coach', price: 125000 },
+          { name: 'Business', price: 275000 },
+          { name: 'First', price: 430000 }
+        ]
+      },
+      bookings: [{ name: 'Sample', price: 0 }],
+      user: {
+        name: 'Cheryl',
+        age: 28
+      }
+    }
   },
-  methods: {}
+  components: {
+    UserDisplay
+  }
 })
 </script>
 
 <style lang="css" scoped>
-  .home-page {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-  }
+body {
+  background-color: #f2f2f2;
+  margin: 0, 5%;
+  font-family: tahoma;
+}
+
+.row {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  vertical-align: middle;
+  margin: 2em;
+}
+
+.button {
+  background-color: #39495c;
+  border-radius: 5px;
+  color: white;
+  text-align: center;
+}
+
+.nav-bar {
+  background: linear-gradient(-50deg, #010801, #0d0d60);
+  height: 60px;
+  margin-bottom: 25px;
+}
 </style>
---
+++ vue-project/src/components/user/UserDisplay.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="user-box">
+    <h2>{{ user.name }}</h2>
+    <div>{{ user.age }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+interface User {
+  name: string
+  age: number
+}
+export default defineComponent({
+  name: 'UserDisplay',
+  props: {
+    user: {
+      type: Object as () => User,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+.user-box {
+  border: 1px solid #ccc;
+  padding: 1em;
+  margin: 1em;
+  width: 200px;
+}
+</style>
