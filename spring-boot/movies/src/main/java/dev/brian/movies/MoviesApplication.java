c17cafb305f410cdfce3bcf1e179ac7b9ba73e2a
npham49 <nguyenphamswork@gmail.com>
2024-03-16 00:43:40 -0700

    added basic mongodb template and first controller

--- spring-boot/movies/src/main/java/dev/brian/movies/MoviesApplication.java
+++ spring-boot/movies/src/main/java/dev/brian/movies/MoviesApplication.java
@@ -2,8 +2,8 @@ package dev.brian.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
+//This is a method that allows Spring to know this is a REST API
 public class MoviesApplication {
 
 	public static void main(String[] args) {
